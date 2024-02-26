fa871b47b537fc9b2e1ea9294d1999b8c9404024
pl260851 <144284479+pl260851@users.noreply.github.com>
2024-02-25 22:18:15 -0500

    Update image_processing_file.py

--- image_processing_file.py
+++ image_processing_file.py
@@ -2,11 +2,10 @@ import cv2
 import numpy as np
 
 def region_of_interest(img, vertices):
-    # Create a mask with the same dimensions as the image, initialized to zero (black).
+    # Create a mask
     mask = np.zeros_like(img)
-    # Fill the polygon defined by `vertices` with white color (255) on the mask.
     cv2.fillPoly(mask, [vertices], 255)
-    # Apply the mask to the image using bitwise AND, keeping only the region of interest.
+    # Apply the mask to the image, keeping only the region of interest.
     masked = cv2.bitwise_and(img, mask)
     return masked
 
@@ -15,8 +14,8 @@ def process_image_for_curves(img, roi_vertices):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Apply Gaussian blur to smooth the image and reduce noise.
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    # Use Canny edge detection to find edges in the blurred image.
+    # Use Canny edge detection to find edges 
     canny_image = cv2.Canny(blurred, 100, 35)
-    # Apply the region of interest mask to the Canny image to focus on relevant edges.
+    # Apply the region of interest mask to the Canny image
     masked_canny = region_of_interest(canny_image, roi_vertices)
     return masked_canny
